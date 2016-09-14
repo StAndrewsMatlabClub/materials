759814ca926d6f7a29b2e385ef399fcf6660558b
Justin Ales <justin.ales@gmail.com>
2016-09-14 12:08:42 +0100

    File showing useful things to do to make matlab plots more ready for talks/posters/papers

---
+++ changeDefaultPlotting.m
@@ -0,0 +1,9 @@
+
+%Change matlab default plotting options for better publication quality plots
+set(0,'DefaultLineLineWidth',3)
+set(0,'DefaultFigureColor','w')
+set(0,'DefaultAxesFontSize',24)
+set(0,'DefaultTextFontSize',24)
+myCol = [0 0 0; 0 0 0];
+set(0,'DefaultAxesColorOrder',myCol)
+set(0,'DefaultAxesLineStyleOrder',{'-',':'})
