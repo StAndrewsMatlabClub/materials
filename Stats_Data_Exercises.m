e25d504428f2105cd837cad9c2a5fc99873ddff6
abigailrilee <abigailrilee@googlemail.com>
2016-11-01 11:17:25 +0000

    Data for the Basic Stats exercises
    Data for use with the exercises in the Introduction to Basic Statistics
    session on 02/11/16.

---
+++ Stats_Data_Exercises.m
@@ -0,0 +1,33 @@
+%% exercise 1 data
+%the amount of chocolate a person eats in grams per day every day for two 
+%weeks before and after going on a diet
+
+choc1 = [50 45 75 100 125 60 95 35 75 55 60 25 55 65];
+choc2 = [5 5 10 10 25 15 10 35 20 20 15 30 20 30];
+
+%% exercise 2 data
+%weight in kg, height in cm
+
+weight = [63 80 70 89 74 67 100 78 64 72 82 77 64 102 69 93 80 82 77 78];
+height = [156 179 164 182 170 159 190 173 166 176 181 179 158 192 165 187 175 169 163 175];
+
+
+%% exercise 3 data
+%the strength of 18 structural beams, and the alloy that the beam is made
+%of.
+
+strength = [82 86 79 83 84 85  74 82 78 75 76 79  79 79 81 78 82 80];
+alloy = {'st','st','st','st','st','st', ...
+         'al1','al1','al1','al1','al1','al1',...
+         'al2','al2','al2','al2','al2','al2'};
+
+%% exercise 4 data
+%the height in cm that 8 sunflower plants grow to in different conditions
+
+sunh = [81.9 82.3  72.9 75.5  83.0 81.8  72.6 74.0];
+g1 = [1 2 1 2 1 2 1 2]; %soil type: 1 is regular, 2 is with added nutrients (grouping variable 1; X1)
+g2 = {'hi' 'hi' 'lo' 'lo' 'hi' 'hi' 'lo' 'lo'}; %relatively high or low temperature 
+%on day and days surrounding planting outside (grouping variable 2; X2)
+g3 = {'april' 'april' 'april' 'april' 'may' 'may' 'may' 'may'}; %the month the 
+%seeds were planted outside (grouping variable 3; X3)
+
